8690d4b03e758d7c95f888c1bc5eba1f56f5da3b
fry95116 <fry95116@gmail.com>
2025-09-30 15:37:24 +0000

    chore: init server framework

---
+++ server/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1759241581177_1055',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
